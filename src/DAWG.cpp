b87e4f9344630315e1687167bf6f3dd1c9440164
Michael Thelen <mthelen@gmail.com>
2004-12-03 08:22:11 +0000

    Add DAWG.h and DAWG.cpp.  No implementation yet.

---
+++ src/DAWG.cpp
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------------
+// DAWG.cpp
+//
+// A Directed Acyclic Word Graph class.
+//
+// Copyright 2004 Michael W Thelen.  Permission to copy, use, modify, sell and
+// distribute this software is granted provided this copyright notice appears in
+// all copies. This software is provided "as is" without express or implied
+// warranty, and with no claim as to its suitability for any purpose.
+//
+//---------------------------------------------------------------------------
+
+#include "DAWG.h"
---
+++ src/DAWG.h
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------
+// DAWG.h
+//
+// A Directed Acyclic Word Graph class.
+//
+// Copyright 2004 Michael W Thelen.  Permission to copy, use, modify, sell and
+// distribute this software is granted provided this copyright notice appears in
+// all copies. This software is provided "as is" without express or implied
+// warranty, and with no claim as to its suitability for any purpose.
+//
+//---------------------------------------------------------------------------
+
+class DAWG
+{
+
+};
