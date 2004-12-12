e805147a7f7f1c5d9ca996fd988288a0193ae8c8
Michael Thelen <mthelen@gmail.com>
2004-12-12 07:31:50 +0000

    Remove commented-out code.

--- src/DAWG.cpp
+++ src/DAWG.cpp
@@ -196,17 +196,12 @@ DAWG::getAnagrams (const QString& input, bool subanagrams) const
 
     while (node) {
         c = node->letter;
-//        qDebug ("Looking at node |" + QString (c) + "|, unmatched |"
-//            + unmatched + "|");
 
         int index = unmatched.find (c);
         if (index < 0)
             index = unmatched.find ("?");
 
         if (index >= 0) {
-//            qDebug ("Letter |" + QString (c) + "| found at index "
-//                + QString::number (index) + " in unmatched string |"
-//                + unmatched + "|");
             if (node->next)
                 nodeStack.push (make_pair (node->next,
                                            make_pair (i, unmatched)));
