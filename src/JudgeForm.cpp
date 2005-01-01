1eccdfe00bf6bb7e905ae3483484cbb3c3413698
Michael Thelen <mthelen@gmail.com>
2005-01-01 09:22:18 +0000

    Make Zyzzyva available under the GNU General Public License, Version 2.  Add notices to the beginning of all source files, and add a COPYING file containing the text of the GPL.  Also add an AUTHORS file.

--- src/JudgeForm.cpp
+++ src/JudgeForm.cpp
@@ -3,11 +3,21 @@
 //
 // A form for looking up words.
 //
-// Copyright 2004 Michael W Thelen.  Permission to copy, use, modify, sell and
-// distribute this software is granted provided this copyright notice appears in
-// all copies. This software is provided "as is" without express or implied
-// warranty, and with no claim as to its suitability for any purpose.
+// Copyright 2004, 2005 Michael W Thelen <mike@pietdepsi.com>.
 //
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //---------------------------------------------------------------------------
 
 #include "JudgeForm.h"
