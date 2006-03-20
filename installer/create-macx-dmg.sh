308f7668c9f40007ba7d060919e879060631fa37
Michael Thelen <mthelen@gmail.com>
2006-03-20 23:34:32 +0000

    Change installer to use Qt 4.1.1 instead of Qt 4.1.0.

--- installer/create-macx-dmg.sh
+++ installer/create-macx-dmg.sh
@@ -23,7 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #-----------------------------------------------------------------------------
 
-QTDIR=/usr/local/Trolltech/Qt-4.1.0
+QTDIR=/usr/local/Trolltech/Qt-4.1.1
 QTVER=4.0
 
 # Copy data directory into bundle
