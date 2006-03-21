07e6f96586e08367efc54fb75a59f1c944f862ab
Michael Thelen <mthelen@gmail.com>
2006-03-21 20:46:14 +0000

    Um, somehow IntroForm.h and IntroForm.cpp didn't get added? Well, they're added now.

---
+++ src/libzyzzyva/IntroForm.cpp
@@ -0,0 +1,44 @@
+//---------------------------------------------------------------------------
+// IntroForm.cpp
+//
+// A form for displaying introductory help material.
+//
+// Copyright 2006 Michael W Thelen <mike@pietdepsi.com>.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//---------------------------------------------------------------------------
+
+#include "IntroForm.h"
+#include "Auxil.h"
+#include <QVBoxLayout>
+
+//---------------------------------------------------------------------------
+//  IntroForm
+//
+//! Constructor.
+//
+//! @param parent the parent widget
+//! @param f widget flags
+//---------------------------------------------------------------------------
+IntroForm::IntroForm (QWidget* parent, Qt::WFlags f)
+    : ActionForm (IntroFormType, parent, f)
+{
+    QVBoxLayout* layout = new QVBoxLayout (this);
+
+    browser = new QTextBrowser (this);
+    Q_CHECK_PTR (browser);
+    browser->setSource (Auxil::getHelpDir() + "/index.html");
+    layout->addWidget (browser);
+}
---
+++ src/libzyzzyva/IntroForm.h
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------
+// IntroForm.h
+//
+// A form for displaying introductory help material.
+//
+// Copyright 2006 Michael W Thelen <mike@pietdepsi.com>.
+//
+// This file is part of Zyzzyva.
+//
+// Zyzzyva is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// Zyzzyva is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//---------------------------------------------------------------------------
+
+#ifndef ZYZZYVA_INTRO_FORM_H
+#define ZYZZYVA_INTRO_FORM_H
+
+#include "ActionForm.h"
+#include <QTextBrowser>
+
+class WordEngine;
+
+class IntroForm : public ActionForm
+{
+    Q_OBJECT
+    public:
+    IntroForm (QWidget* parent = 0, Qt::WFlags f = 0);
+    ~IntroForm() { }
+    QString getStatusString() const { return QString::null; }
+
+    private:
+    QTextBrowser* browser;
+};
+
+#endif // ZYZZYVA_INTRO_FORM_H
