d75c9b6956eec0acd44706e233747e5d1bc85193
Michael Thelen <mthelen@gmail.com>
2007-02-23 07:00:27 +0000

    Upgrade to Qt 4.1.2.

--- installer/create-macx-dmg.sh
+++ installer/create-macx-dmg.sh
@@ -23,7 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #-----------------------------------------------------------------------------
 
-QTDIR=/usr/local/Trolltech/Qt-4.2.1
+QTDIR=/usr/local/Trolltech/Qt-4.2.2
 QTVER=4
 OUTDIR=$(pwd)/installer/macosx
 
