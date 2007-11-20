72addbf87b57a7e3f01a6103e2a18dec04983dfd
Michael Thelen <mthelen@gmail.com>
2007-11-20 00:00:04 +0000

    Bump version from 1.0.5 to 1.0.6.

--- installer/create-macx-installer-dmg.sh
+++ installer/create-macx-installer-dmg.sh
@@ -23,7 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #-----------------------------------------------------------------------------
 
-ZVER=1.0.5
+ZVER=1.0.6
 OUTDIR=$HOME/dev/zyzzyva-install
 DMGFILE=$OUTDIR/Zyzzyva-$ZVER-osx-installer.dmg
 
