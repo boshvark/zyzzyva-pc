1147eb9286f1db63aa33e6ff20bef96f3aaf4251
Michael Thelen <mthelen@gmail.com>
2008-02-24 05:00:36 +0000

    Add Hack.h to contain temporary hacks.

---
+++ src/libzyzzyva/Hack.h
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------------
+// Hack.h
+//
+// A file for temporary hacks.  If any source files actually include this
+// file, something is wrong.
+//
+// Copyright 2008 Michael W Thelen <mthelen@gmail.com>.
+//
+// This file is part of Zyzzyva.
+//
+// Zyzzyva is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// Zyzzyva is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//---------------------------------------------------------------------------
+
+#ifndef ZYZZYVA_HACK_H
+#define ZYZZYVA_HACK_H
+
+#include <QString>
+
+namespace Hack {
+    const QString LEXICON = "OWL2+LWL";
+}
+
+#endif // ZYZZYVA_HACK_H
