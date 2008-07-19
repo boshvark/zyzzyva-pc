b7a6b8f0febb134adc431a1ffcc1fd5870a9f8e6
Michael Thelen <mthelen@gmail.com>
2008-07-19 05:49:34 +0000

    Copy libqgif.dylib plugin into OS X bundle.  The program will need to be updated to look for plugins in the appropriate spot, to ensure that duplicate Qt binaries are not loaded from the bundle and the system.

--- installer/create-macx-bundle.sh
+++ installer/create-macx-bundle.sh
@@ -28,8 +28,9 @@ set -e
 QTDIR=/usr/local/Trolltech/Qt-4.3.2
 QTVER=4
 OUTDIR=$(pwd)/installer/macosx
+APPDIR=$OUTDIR/Zyzzyva.app
 
-if [ ! -e $OUTDIR/Zyzzyva.app/Contents/Frameworks ]; then
+if [ ! -e $APPDIR/Contents/Frameworks ]; then
     COPYQT="yes"
 fi
 
@@ -38,43 +39,42 @@ $QTDIR/bin/qmake
 echo "Building Zyzzyva..."
 make -s
 
-mkdir -p $OUTDIR/Zyzzyva.app/Contents/Frameworks
+mkdir -p $APPDIR/Contents/Frameworks
 
 # Copy executable into bundle
 echo "Copying Zyzzyva executable into bundle..."
-cp -r bin/Zyzzyva.app/Contents/* $OUTDIR/Zyzzyva.app/Contents
+cp -r bin/Zyzzyva.app/Contents/* $APPDIR/Contents
 
 # Change link location for libzyzzyva in executable
 echo "Changing link location for libzyzzyva in Zyzzyva executable..."
 install_name_tool -change \
     libzyzzyva.2.dylib \
     @executable_path/../Frameworks/libzyzzyva.2.dylib \
-    $OUTDIR/Zyzzyva.app/Contents/MacOS/Zyzzyva
+    $APPDIR/Contents/MacOS/Zyzzyva
 
 # Copy zyzzyva.tap unless it's already there
-if [ ! -e $OUTDIR/Zyzzyva.app/Contents/MacOS/zyzzyva.top ]; then
+if [ ! -e $APPDIR/Contents/MacOS/zyzzyva.top ]; then
     echo "Copying zyzzyva.top into bundle..."
-    cp zyzzyva.top $OUTDIR/Zyzzyva.app/Contents/MacOS
+    cp zyzzyva.top $APPDIR/Contents/MacOS
 fi
 
 # Copy data directory into bundle unless it's already there
-if [ ! -e $OUTDIR/Zyzzyva.app/Contents/MacOS/data ]; then
+if [ ! -e $APPDIR/Contents/MacOS/data ]; then
     echo "Copying data directory into bundle..."
-    cp -r data $OUTDIR/Zyzzyva.app/Contents/MacOS
+    cp -r data $APPDIR/Contents/MacOS
     find $OUTDIR -type d -name '.svn' -print0 | xargs -0 rm -rf
 fi
 
 # Copy Zyzzyva libs to Frameworks directory
 echo "Copying libzyzzyva into bundle..."
-cp bin/libzyzzyva.2.dylib $OUTDIR/Zyzzyva.app/Contents/Frameworks
+cp bin/libzyzzyva.2.dylib $APPDIR/Contents/Frameworks
 
 # Copy Qt libs to Frameworks directory unless they're already there
 if [ "$COPYQT" = "yes" ]; then
 
     # Copy Assistant client into bundle
     echo "Copying Assistant client into bundle..."
-    cp -r $QTDIR/bin/assistant.app \
-        $OUTDIR/Zyzzyva.app/Contents/MacOS
+    cp -r $QTDIR/bin/assistant.app $APPDIR/Contents/MacOS
 
     # Copy Qt frameworks into bundle and tell the executable to link to them
     for i in QtCore QtGui QtNetwork QtSql QtXml QtAssistant ; do
@@ -82,21 +82,21 @@ if [ "$COPYQT" = "yes" ]; then
         # Copy Qt framework into bundle
         echo "Copying $i.framework into bundle..."
         pushd $QTDIR/lib > /dev/null
-        tar -cf - ./$i.framework | (cd $OUTDIR/Zyzzyva.app/Contents/Frameworks && tar -xf -)
+        tar -cf - ./$i.framework | (cd $APPDIR/Contents/Frameworks && tar -xf -)
         popd > /dev/null
 
         # Delete headers and debug libraries
         echo "Deleting $i.framework headers and debug libraries..."
-        rm $OUTDIR/Zyzzyva.app/Contents/Frameworks/$i.framework/Headers
-        rm -rf $OUTDIR/Zyzzyva.app/Contents/Frameworks/$i.framework/Versions/$QTVER/Headers
-        find $OUTDIR/Zyzzyva.app/Contents/Frameworks -name arch -or -name '*debug*' \
+        rm $APPDIR/Contents/Frameworks/$i.framework/Headers
+        rm -rf $APPDIR/Contents/Frameworks/$i.framework/Versions/$QTVER/Headers
+        find $APPDIR/Contents/Frameworks -name arch -or -name '*debug*' \
             -print0 | xargs -0 rm -rf
 
         # Set identification names for the private framework
         echo "Setting identification name for $i.framework..."
         install_name_tool \
             -id @executable_path/../Frameworks/$i.framework/Versions/$QTVER/$i \
-            $OUTDIR/Zyzzyva.app/Contents/Frameworks/$i.framework/Versions/$QTVER/$i
+            $APPDIR/Contents/Frameworks/$i.framework/Versions/$QTVER/$i
 
         #  Change reference to QtCore in frameworks
         if [ "$i" != "QtCore" ]; then
@@ -104,8 +104,36 @@ if [ "$COPYQT" = "yes" ]; then
             install_name_tool -change \
                 $QTDIR/lib/QtCore.framework/Versions/$QTVER/QtCore \
                 @executable_path/../Frameworks/QtCore.framework/Versions/$QTVER/QtCore \
-                $OUTDIR/Zyzzyva.app/Contents/Frameworks/$i.framework/Versions/$QTVER/$i
+                $APPDIR/Contents/Frameworks/$i.framework/Versions/$QTVER/$i
         fi
+
+    done
+
+    mkdir -p $APPDIR/Contents/PlugIns
+
+    # Copy plugins into bundle and update references
+    for i in imageformats/libqgif.dylib ; do
+
+        # Copy plugin into bundle
+        echo "Copying $i into bundle..."
+
+        PLUGPATH=${i%/*}
+        mkdir -p $APPDIR/Contents/PlugIns/$PLUGPATH
+        cp $QTDIR/plugins/$i $APPDIR/Contents/PlugIns/$PLUGPATH
+
+        #  Change reference to QtCore in plugin
+        echo "Changing link location for QtCore.framework in $i plugin..."
+        install_name_tool -change \
+            $QTDIR/lib/QtCore.framework/Versions/$QTVER/QtCore \
+            @executable_path/../Frameworks/QtCore.framework/Versions/$QTVER/QtCore \
+            $APPDIR/Contents/PlugIns/$i
+
+        #  Change reference to QtGui in plugin
+        echo "Changing link location for QtGui in $i plugin..."
+        install_name_tool -change \
+            $QTDIR/lib/QtGui.framework/Versions/$QTVER/QtGui \
+            @executable_path/../Frameworks/QtGui.framework/Versions/$QTVER/QtGui \
+            $APPDIR/Contents/PlugIns/$i
 
     done
 
@@ -114,21 +142,21 @@ if [ "$COPYQT" = "yes" ]; then
     install_name_tool -change \
         $QTDIR/lib/QtGui.framework/Versions/$QTVER/QtGui \
         @executable_path/../Frameworks/QtGui.framework/Versions/$QTVER/QtGui \
-        $OUTDIR/Zyzzyva.app/Contents/Frameworks/QtAssistant.framework/Versions/$QTVER/QtAssistant
+        $APPDIR/Contents/Frameworks/QtAssistant.framework/Versions/$QTVER/QtAssistant
 
     # Change reference to QtNetwork in Assistant client
     echo "Changing link location for QtNetwork.framework in QtAssistant.framework..."
     install_name_tool -change \
         $QTDIR/lib/QtNetwork.framework/Versions/$QTVER/QtNetwork \
         @executable_path/../Frameworks/QtNetwork.framework/Versions/$QTVER/QtNetwork \
-        $OUTDIR/Zyzzyva.app/Contents/Frameworks/QtAssistant.framework/Versions/$QTVER/QtAssistant
+        $APPDIR/Contents/Frameworks/QtAssistant.framework/Versions/$QTVER/QtAssistant
 
     # Change reference to QtCore in Assistant client
     echo "Changing link location for QtCore.framework in QtAssistant.framework..."
     install_name_tool -change \
         $QTDIR/lib/QtCore.framework/Versions/$QTVER/QtCore \
         @executable_path/../Frameworks/QtCore.framework/Versions/$QTVER/QtCore \
-        $OUTDIR/Zyzzyva.app/Contents/Frameworks/QtAssistant.framework/Versions/$QTVER/QtAssistant
+        $APPDIR/Contents/Frameworks/QtAssistant.framework/Versions/$QTVER/QtAssistant
 
 fi
 
@@ -140,15 +168,15 @@ for i in QtCore QtGui QtNetwork QtSql QtXml QtAssistant ; do
     install_name_tool -change \
         $QTDIR/lib/$i.framework/Versions/$QTVER/$i \
         @executable_path/../Frameworks/$i.framework/Versions/$QTVER/$i \
-        $OUTDIR/Zyzzyva.app/Contents/Frameworks/libzyzzyva.2.dylib
+        $APPDIR/Contents/Frameworks/libzyzzyva.2.dylib
 
     # Change link locations for framework in executable
     echo "Changing link location for $i.framework in Zyzzyva executable..."
     install_name_tool -change \
         $QTDIR/lib/$i.framework/Versions/$QTVER/$i \
         @executable_path/../Frameworks/$i.framework/Versions/$QTVER/$i \
-        $OUTDIR/Zyzzyva.app/Contents/MacOS/Zyzzyva
+        $APPDIR/Contents/MacOS/Zyzzyva
 
 done
 
-echo "Done.  Bundle is called $OUTDIR/Zyzzyva.app."
+echo "Done.  Bundle is called $APPDIR."
