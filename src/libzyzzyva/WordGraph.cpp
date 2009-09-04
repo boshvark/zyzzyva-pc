c248b690345929590874f68f273573f07108a61a
Michael Thelen <mthelen@gmail.com>
2009-09-04 21:42:16 +0000

    Merge r2529 from Zyzzyva trunk. Patch by Nikolas Coukouma <atrus@atrus.org>. Fix a compile error on gcc 4.4.1 by using qint32 instead of int32_t.

--- src/libzyzzyva/WordGraph.cpp
+++ src/libzyzzyva/WordGraph.cpp
@@ -31,17 +31,17 @@
 #include <map>
 #include <stack>
 
-const int32_t TERMINAL_NODE = 0;
-const int32_t ROOT_NODE = 1;
+const qint32 TERMINAL_NODE = 0;
+const qint32 ROOT_NODE = 1;
 
-const int32_t V_END_OF_WORD = 23;
-const int32_t M_END_OF_WORD = (1L << V_END_OF_WORD);
-const int32_t V_END_OF_NODE = 22;
-const int32_t M_END_OF_NODE = (1L << V_END_OF_NODE);
+const qint32 V_END_OF_WORD = 23;
+const qint32 M_END_OF_WORD = (1L << V_END_OF_WORD);
+const qint32 V_END_OF_NODE = 22;
+const qint32 M_END_OF_NODE = (1L << V_END_OF_NODE);
 
-const int32_t V_LETTER       = 24;
-const int32_t M_LETTER       = 0xFF;
-const int32_t M_NODE_POINTER = 0x1FFFFFL;
+const qint32 V_LETTER       = 24;
+const qint32 M_LETTER       = 0xFF;
+const qint32 M_NODE_POINTER = 0x1FFFFFL;
 
 using namespace std;
 using namespace Defs;
@@ -109,26 +109,26 @@ WordGraph::importDawgFile(const QString& filename, bool reverse, QString*
         return false;
     }
 
-    int32_t numEdges;
-    int32_t* p = &numEdges;
+    qint32 numEdges;
+    qint32* p = &numEdges;
     char* cp = (char*) p;
-    file.read(cp, 1 * sizeof(int32_t));
+    file.read(cp, 1 * sizeof(qint32));
     if (bigEndian)
         convertEndian(p, 1);
 
     if (reverse) {
-        rdawg = new int32_t[numEdges + 1];
+        rdawg = new qint32[numEdges + 1];
         rdawg[0] = 0;
         p = &rdawg[1];
         cp = (char*) p;
-        file.read(cp, numEdges * sizeof(int32_t));
+        file.read(cp, numEdges * sizeof(qint32));
     }
     else {
-        dawg = new int32_t[numEdges + 1];
+        dawg = new qint32[numEdges + 1];
         dawg[0] = 0;
         p = &dawg[1];
         cp = (char*) p;
-        file.read(cp, numEdges * sizeof(int32_t));
+        file.read(cp, numEdges * sizeof(qint32));
     }
 
     if (expectedChecksum && errString) {
@@ -181,7 +181,7 @@ WordGraph::containsWord(const QString& w) const
     if (!dawg)
         return containsWordOld(w);
 
-    int32_t node = ROOT_NODE;
+    qint32 node = ROOT_NODE;
     bool eow = false;
 
     for (int i = 0; i < w.length(); ++i) {
@@ -189,8 +189,8 @@ WordGraph::containsWord(const QString& w) const
             return false;
 
         QChar letter = w.at(i);
-        for (int32_t* edge = &dawg[node]; ; ++edge) {
-            int32_t lc = *edge;
+        for (qint32* edge = &dawg[node]; ; ++edge) {
+            qint32 lc = *edge;
             lc = lc >> V_LETTER;
             lc = lc & M_LETTER;
             char c = (char) lc;
@@ -332,7 +332,7 @@ WordGraph::search(const SearchSpec& spec) const
             }
         }
 
-        int32_t node = ROOT_NODE;
+        qint32 node = ROOT_NODE;
 
         // Traverse the tree looking for matches
         while (node) {
@@ -361,11 +361,11 @@ WordGraph::search(const SearchSpec& spec) const
                     }
                 }
 
-                int32_t* edge = reversePattern ? &rdawg[node] : &dawg[node];
+                qint32* edge = reversePattern ? &rdawg[node] : &dawg[node];
 
                 // Traverse next nodes, looking for matches
                 for (; ; ++edge, ++node) {
-                    int32_t longLetter = *edge;
+                    qint32 longLetter = *edge;
                     longLetter = longLetter >> V_LETTER;
                     longLetter = longLetter & M_LETTER;
 
@@ -402,7 +402,7 @@ WordGraph::search(const SearchSpec& spec) const
                                 continue;
                         }
 
-                        int32_t child = *edge & M_NODE_POINTER;
+                        qint32 child = *edge & M_NODE_POINTER;
 
                         // If this node matches, push its child on the stack
                         // to be traversed later
@@ -473,7 +473,7 @@ WordGraph::search(const SearchSpec& spec) const
 
                                 else if (c == ']') {
                                     if (found ^ negated) {
-                                        int32_t child = *edge & M_NODE_POINTER;
+                                        qint32 child = *edge & M_NODE_POINTER;
 
                                         if (matchEnd < 0) {
                                             matchStart = groupStart;
@@ -530,7 +530,7 @@ WordGraph::search(const SearchSpec& spec) const
                                                   matchEnd - matchStart + 1,
                                                   QString());
 
-                            int32_t child = *edge & M_NODE_POINTER;
+                            qint32 child = *edge & M_NODE_POINTER;
                             if (child &&
                                 (wildcard || !unmatched.isEmpty()))
                             {
@@ -685,8 +685,8 @@ WordGraph::matchesSpec(QString word, const SearchSpec& spec) const
 //! @param count the number of 32-bit words to convert
 //! @return the number of words converted
 //---------------------------------------------------------------------------
-int32_t
-WordGraph::convertEndian(int32_t* data, int32_t count)
+qint32
+WordGraph::convertEndian(qint32* data, qint32 count)
 {
     for (int i = 0; i < count; ++i) {
         char* p = (char*) data;
@@ -1198,10 +1198,10 @@ WordGraph::searchOld(const SearchSpec& spec) const
 //! @return the number of words
 //---------------------------------------------------------------------------
 int
-WordGraph::getNumWords(int32_t node) const
+WordGraph::getNumWords(qint32 node) const
 {
     int count = 0;
-    for (int32_t* edge = &dawg[node]; ; ++edge) {
+    for (qint32* edge = &dawg[node]; ; ++edge) {
         if ((*edge & M_END_OF_WORD) != 0)
             ++count;
         node = *edge & M_NODE_POINTER;
