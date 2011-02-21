3283f2f1c4bc2b1424025724e77b8e28ceeaec71
Michael Thelen <mthelen@gmail.com>
2011-02-21 06:08:19 +0000

    Revert part of r2731 that was erroneously applied to 2.1.x.

--- src/zyzzyva/zyzzyva.cpp
+++ src/zyzzyva/zyzzyva.cpp
@@ -40,10 +40,6 @@ int main(int argc, char** argv)
     QCoreApplication::setOrganizationDomain(SETTINGS_DOMAIN_NAME);
     QCoreApplication::setApplicationName(SETTINGS_APPLICATION_NAME);
 
-#if defined Z_OSX
-    qApp->setLibraryPath(qApp->applicationDirPath() + "/../PlugIns");
-#endif
-
     QPixmap pixmap (":/zyzzyva-splash");
     QSplashScreen* splash = new QSplashScreen(pixmap);
     splash->show();
