abe19721fb9f303bb8ed9618bec52f0dc7220016
Michael Thelen <mthelen@gmail.com>
2012-04-24 22:09:15 -0600

    Restore code to use zyzzyva-32x32.png.

--- src/libzyzzyva/MainWindow.cpp
+++ src/libzyzzyva/MainWindow.cpp
@@ -373,7 +373,7 @@ MainWindow::MainWindow(QWidget* parent, QSplashScreen* splash, Qt::WFlags f)
     makeUserDirs();
 
     setWindowTitle(APPLICATION_TITLE);
-    setWindowIcon(QIcon(":/zyzzyva-icon"));
+    setWindowIcon(QIcon(":/zyzzyva-32x32"));
 
     if (!instance)
         instance = this;
