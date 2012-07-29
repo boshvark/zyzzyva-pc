546828292feeebffe1fb1b450f72e4456411ff95
Michael Thelen <mthelen@gmail.com>
2012-07-29 16:46:13 -0600

    Only highlight input after search if pref is set.
    ZD-508.

--- src/libzyzzyva/SearchForm.cpp
+++ src/libzyzzyva/SearchForm.cpp
@@ -336,7 +336,7 @@ SearchForm::search()
 
     QWidget* focusWidget = QApplication::focusWidget();
     QLineEdit* lineEdit = dynamic_cast<QLineEdit*>(focusWidget);
-    if (lineEdit) {
+    if (lineEdit && MainSettings::getSearchSelectInput()) {
         lineEdit->setSelection(0, lineEdit->text().length());
     }
     else {
