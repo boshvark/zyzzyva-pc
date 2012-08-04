4bbbd87950b9d63c9e34b77995f84e62dc97ace7
Michael Thelen <mthelen@gmail.com>
2012-08-03 23:24:22 -0600

    Fill Cardbox tab when opened.

--- src/libzyzzyva/CardboxForm.cpp
+++ src/libzyzzyva/CardboxForm.cpp
@@ -150,6 +150,7 @@ CardboxForm::CardboxForm(WordEngine* e, QWidget* parent, Qt::WFlags f)
     //refreshTimer.start(REFRESH_MSECS);
 
     lexiconActivated(lexiconWidget->getCurrentLexicon());
+    refreshClicked();
 }
 
 //---------------------------------------------------------------------------
